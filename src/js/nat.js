5e245b4761cd90c46ab8b83d2207d6da71ce9b05
Dmitriy267 <dmitrio87@mail.ru>
2026-01-23 20:39:51 +0300

    feat: увеличение и растворение с вращением

--- src/js/nat.js
+++ src/js/nat.js
@@ -67,11 +67,60 @@
 // Конец кода гасим лишнее
 
 //Выбор фото из списка
-addEventListener('load', () => {
-    document.getElementById('sel').addEventListener('change', () => {
-        let a = document.getElementById('sel').value;
-        document.getElementById('photo').src =
-            '/images/nature/nat' + a + '.png';
-    });
-});
+// addEventListener('load', () => {
+//     document.getElementById('sel').addEventListener('change', () => {
+//         let a = document.getElementById('sel').value;
+//         document.getElementById('photo').src =
+//             '/images/nature/nat' + a + '.png';
+//     });
+// });
 // Конец Выбор фото из списка
+
+//Увеличение и растворение
+let y = 1;
+let w = 0;
+let h = 0;
+addEventListener('click', (ev) => {
+    let e = ev.target;
+    if (e.tagName == 'IMG') {
+        let t = document.getElementById(e.id).style;
+        if (y == 1) {
+            let d = document.getElementById(e.id);
+            w = d.offsetLeft;
+            console.log(w);
+            h = d.offsetTop;
+            console.log(h);
+            y = 2;
+            for (let i = 1; i < 7; i++) {
+                let b = 'i' + i;
+                if (e.id != b) {
+                    let p = document.getElementById(b).style;
+                    p.transition = 'opacity 1s';
+                    p.opacity = 0;
+                }
+            }
+            t.zIndex = 2;
+            t.transition = 'width 1s, left 1s, top 1s, transform 1s';
+            t.transform = 'rotate(360deg)';
+            t.width = 600 + 'px';
+            t.left = 200 + 'px';
+            t.top = 100 + 'px';
+        } else {
+            y = 1;
+            for (let i = 1; i < 7; i++) {
+                let b = 'i' + i;
+
+                let p = document.getElementById(b).style;
+                p.transition = 'opacity 2s';
+                p.opacity = 1;
+            }
+            t.transition = 'width 1s, left 1s, top 1s, transform 1s';
+            t.transform = 'rotate(0deg)';
+            t.width = 300 + 'px';
+            t.left = w + 'px';
+            t.top = h + 'px';
+            t.zIndex = 1;
+        }
+    }
+});
+//Конец увеличения и растворения
