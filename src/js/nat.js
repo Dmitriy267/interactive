e8c91517f0cfae743d17f0e4dd636f0e3266ec41
Dmitriy267 <dmitrio87@mail.ru>
2026-03-17 15:06:06 +0300

    удалил лишнее

--- src/js/nat.js
+++ src/js/nat.js
@@ -1,7 +1,10 @@
-//Просмотр фотографий
-// addEventListener('load', function () {
-//     addEventListener('scroll', del);
+/*Простой просмотр*/
+document.getElementById('pict61').addEventListener('click', function () {
+    document.getElementById('pict61').requestFullscreen();
+});
+/* Конец Простой просмотр*/
 
+/*Просмотр фотографий*/
 const imtranspAll = document.querySelectorAll('.imtransp');
 const imtransp = Array.from(imtranspAll);
 
@@ -28,52 +31,7 @@ imtransp.forEach((item) => {
     });
 });
 
-//     let view = document.getElementById('view');
-//     let bas = document.getElementById('bas');
-//     let pict = document.getElementById('pict');
-
-//     view.addEventListener('click', del);
-//     document.getElementById('basis').addEventListener('click', function (e) {
-//         if (e.target.tagName === 'IMG') {
-//             let sc = window.pageYOffset;
-//             bas.style.top = sc + 'px';
-//             view.style.top = sc + 'px';
-//             pict.src = e.target.src;
-//             view.style.visibility = 'visible';
-//             bas.style.visibility = 'visible';
-//         }
-//     });
-
-//     function del() {
-//         view.style.visibility = 'hidden';
-//         bas.style.visibility = 'hidden';
-//         pict.src = '/public/net.png';
-//     }
-// });
-// Конец кода Просмотр фотографий
-/*Гасим лишнее*/
-
-document.addEventListener('mouseover', function (e) {
-    let ev = e.target;
-    if (ev.tagName === 'IMG') {
-        for (let i = 1; i < 10; i++) {
-            let b = 'x' + i;
-            if (ev.id != b) {
-                let t = document.getElementById(b).style;
-                t.transition = 'opacity 1s';
-                t.opacity = 0.1;
-            }
-        }
-    } else {
-        for (let i = 1; i < 10; i++) {
-            let b = 'x' + i;
-            let t = document.getElementById(b).style;
-            t.transition = 'opacity 2s';
-            t.opacity = 1;
-        }
-    }
-});
-/* Конец кода гасим лишнее*/
+/* Конец кода Просмотр фотографий */
 
 /*Выбор из списка*/
 
@@ -83,134 +41,6 @@ document.getElementById('sel').addEventListener('change', () => {
 });
 
 /* Конец Выбор  из списка*/
-
-//Увеличение и растворение
-// let y = 1;
-// let w = 0;
-// let h = 0;
-// addEventListener('click', (ev) => {
-//     let e = ev.target;
-//     if (e.tagName == 'IMG') {
-//         let t = document.getElementById(e.id).style;
-//         if (y == 1) {
-//             let d = document.getElementById(e.id);
-//             w = d.offsetLeft;
-//             console.log(w);
-//             h = d.offsetTop;
-//             console.log(h);
-//             y = 2;
-//             for (let i = 1; i < 7; i++) {
-//                 let b = 'i' + i;
-//                 if (e.id != b) {
-//                     let p = document.getElementById(b).style;
-//                     p.transition = 'opacity 1s';
-//                     p.opacity = 0;
-//                 }
-//             }
-//             t.zIndex = 2;
-//             t.transition = 'width 1s, left 1s, top 1s, transform 1s';
-//             t.transform = 'rotate(360deg)';
-//             t.width = 600 + 'px';
-//             t.left = 200 + 'px';
-//             t.top = 100 + 'px';
-//         } else {
-//             y = 1;
-//             for (let i = 1; i < 7; i++) {
-//                 let b = 'i' + i;
-
-//                 let p = document.getElementById(b).style;
-//                 p.transition = 'opacity 2s';
-//                 p.opacity = 1;
-//             }
-//             t.transition = 'width 1s, left 1s, top 1s, transform 1s';
-//             t.transform = 'rotate(0deg)';
-//             t.width = 300 + 'px';
-//             t.left = w + 'px';
-//             t.top = h + 'px';
-//             t.zIndex = 1;
-//         }
-//     }
-// });
-//Конец увеличения и растворения
-
-//Список альбомов
-// addEventListener('load', function () {
-//     let arr = [
-//         [1, 2, 14, 20, 30, 70],
-//         [18, 22, 29, 35, 60, 74],
-//         [3, 6, 13, 27, 88, 89],
-//     ];
-//     document.getElementById('sel').addEventListener('change', () => {
-//         document.getElementById('ins').innerHTML = '';
-//         let a = document.getElementById('sel').value;
-
-//         if (a != 99) {
-//             let summ = '';
-//             for (let i = 0; i < arr[a].length; i++) {
-//                 summ =
-//                     summ +
-//                     '<img src="/images/albom/' +
-//                     arr[a][i] +
-//                     '.jpg" class="im" alt="Фото" >';
-//                 document.getElementById('ins').innerHTML = summ;
-//             }
-//         }
-//     });
-// });
-// addEventListener('scroll', del);
-// let bas = document.getElementById('bas').style;
-// let view = document.getElementById('view');
-// let pict = document.getElementById('pict');
-// document.getElementById('basis').addEventListener('click', (ev) => {
-//     let e = ev.target;
-//     if (e.tagName === 'IMG') {
-//         let sc = window.pageYOffset;
-
-//         bas.top = sc + 'px';
-//         view.style.top = sc + 'px';
-//         pict.src = e.src;
-//         view.style.visibility = 'visible';
-//         bas.visibility = 'visible';
-//     }
-// });
-// view.addEventListener('click', del);
-// function del() {
-//     view.style.visibility = 'hidden';
-
-//     bas.visibility = 'hidden';
-//     pict.src = '/images/albom/net.jpg';
-// }
-//Конец списка альбомов
-
-// Слои вместо фото
-// addEventListener('load', function () {
-//     document.getElementById('basis').addEventListener('click', look);
-//     document.getElementById('view').addEventListener('click', del);
-//     addEventListener('scroll', del);
-
-//     let bas = document.getElementById('bas').style;
-//     let view = document.getElementById('view').style;
-//     let pict = document.getElementById('pict');
-
-//     function look(ev) {
-//         let ni = ev.target.id;
-
-//         if (ni.indexOf('fr') == 0) {
-//             let sc = window.pageYOffset;
-//             bas.top = sc + 'px';
-//             view.top = sc + 'px';
-//             pict.src = '/images/berries/' + ni + '.jpg';
-//             view.visibility = 'visible';
-//             bas.visibility = 'visible';
-//         }
-//     }
-//     function del() {
-//         view.visibility = 'hidden';
-//         bas.visibility = 'hidden';
-//         pict.src = '/images/berries/net.jpg';
-//     }
-// });
-//Конец Слои вместо фото
 
 //Галереи
 //Смещаем фото вверх
@@ -247,7 +77,7 @@ function but() {
     }
 }
 function mov() {
-    if (k >= -700) {
+    if (k >= -2000) {
         document.getElementById('i' + s).style.top = k + 'px';
         k = k - 10;
         setTimeout(mov, 10);
@@ -306,19 +136,20 @@ function next() {
 }
 // Конец Перемотка от центра
 //Галерея-аккардион
-let i96 = 1;
+let iAccord = 1;
 
 document.getElementById('bu96').addEventListener('click', function () {
-    if (i96 < 6) {
-        document.getElementById('i96' + i96).style.transition = 'width 2s';
-        document.getElementById('i96' + i96).style.width = 0;
-        document.getElementById('i96' + i96).style.left = 0;
+    if (iAccord < 6) {
+        document.getElementById('accord' + iAccord).style.transition =
+            'width 2s';
+        document.getElementById('accord' + iAccord).style.width = 0;
+        document.getElementById('accord' + iAccord).style.left = 0;
     }
-    let n = i96 + 1;
-    document.getElementById('i96' + n).style.transition = 'width 1.95s';
-    document.getElementById('i96' + n).style.width = '600px';
-    i96 = i96 + 1;
-    if (i96 == 6) {
+    let n = iAccord + 1;
+    document.getElementById('accord' + n).style.transition = 'width 1.95s';
+    document.getElementById('accord' + n).style.width = '600px';
+    iAccord = iAccord + 1;
+    if (iAccord == 6) {
         document.getElementById('bu96').value = 'Просмотр завершен';
     }
 });
